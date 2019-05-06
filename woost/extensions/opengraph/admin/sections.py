42a8b8119f01526b4af1b45e279f327fe3a596f8
Marti Congost <marti.congost@whads.com>
2019-05-06 17:22:38 +0200

    Added the missing icon and text for the settings section

--- woost/extensions/opengraph/admin/sections.py
+++ woost/extensions/opengraph/admin/sections.py
@@ -2,11 +2,16 @@
 
 .. moduleauthor:: Martí Congost <marti.congost@whads.com>
 """
+from cocktail.translations import translations
 from woost.admin.sections import Settings
 from woost.admin.sections.contentsection import ContentSection
 
+translations.load_bundle("woost.extensions.opengraph.admin.sections")
+
 
 class OpenGraphSettings(Settings):
+
+    icon_uri = "woost.extensions.opengraph.admin.ui://images/opengraph.svg"
 
     members = [
         "x_opengraph_fb_admins",
